fbf00476a75871f026dd0c13a4b48260f2dbf554
HudsonBlack96 <hudson89.silva@gmail.com>
2024-01-14 15:41:22 +0000

    INICIAL ZERO

---
+++ javascript.js
@@ -0,0 +1,20 @@
+let digimon = {
+    nome : 'Agumon',
+    type : 'Fire',
+    attack : ['Baby flame','Super Pepper Breath','Spirit Fire'],
+    evolutionmax : 'WarGreymon',
+    legendaryDigimon :true
+
+}
+console.log (digimon.nome +' '+ 'attacks the enemy using ' + digimon.attack [0]);
+
+
+let array = {
+    Evolution:['Goromon','Agumon','Greymon','MetalGreymon','skullGreymon','Wargreymon'],
+
+
+}
+
+console.log (digimon)
+console.log (array)
+console.log (digimon.nome + ' '+ 'in battle digivolve to ' + array.Evolution[1]);
